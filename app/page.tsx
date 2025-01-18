42952f4be829aa136a5ff5d3c4e5627f3e33a3d8
saqibtanveer786 <elven5055@gmail.com>
2025-01-18 21:28:55 +0500

    make this project out

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/app/components/header";
+import ChatSection from "./components/chat-section";
+import Link from "next/link";
+
+
+export default async function Home() {
+  return (
+    <main className=" w-full">
+      what is this
+    </main>
+  );
+}
